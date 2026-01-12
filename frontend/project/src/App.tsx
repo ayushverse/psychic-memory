5406bc6c0f3f7e7dc19c24f2516433409850e592
ayushverse <56singh5055@gmail.com>
2026-01-12 22:54:41 +0530

    added Icons

--- frontend/project/src/App.tsx
+++ frontend/project/src/App.tsx
@@ -1,10 +1,12 @@
 import Button from "./components/Button.tsx";
+import {PlusIcon} from "./icons/PlusIcon.tsx";
+import {ShareIcon} from "./icons/ShareIcon.tsx";
 
 function App() {
     return (
         <div>
-            <Button variant={"primary"} size={"sm"} text={"Add Content"} onClick={()=>console.log("clicked")} startIcon={"+"} endIcon={""} />
-            <Button variant={"secondary"} size={"sm"} text={"Share Content"} onClick={()=>console.log("clicked")} startIcon={"<"} endIcon={""} />
+            <Button variant={"primary"} size={"sm"} text={"Add Content"} onClick={()=>console.log("clicked")} startIcon={<PlusIcon/>} endIcon={""} />
+            <Button variant={"secondary"} size={"sm"} text={"Share Content"} onClick={()=>console.log("clicked")} startIcon={<ShareIcon/>} endIcon={""} />
 
         </div>
     );
